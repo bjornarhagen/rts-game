33ef813d91d7c246f1358742fa752685dc06813b
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-08-08 03:10:58 +0200

    Update to start on main menu. Fix main menu

--- src/Main.gd
+++ src/Main.gd
@@ -6,6 +6,8 @@ extends Node
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		var main_menu_scene = preload("res://src/MainMenu.tscn").instance()
-		get_tree().get_root().add_child(main_menu_scene)
-		queue_free()
+		#var main_menu_scene = preload("res://src/MainMenu.tscn").instance()		
+		#get_tree().get_root().add_child(main_menu_scene)
+		#queue_free()
+		#print(get_tree().get_root().get_children())
+		print("TODO: Open menu")
--- src/MainMenu.gd
+++ src/MainMenu.gd
@@ -15,14 +15,11 @@ func empty_selection():
 	selector_settings.text = ""
 	selector_exit.text = ""
 
+# TODO: Fix direction changing bug
 func set_current_selection(direction):
 	empty_selection()
 	current_selection+=direction
-	var processed_selection = abs(current_selection) % number_of_sections
-
-	print(current_selection)
-	print(processed_selection)
-	
+	var processed_selection = abs(current_selection) % number_of_sections	
 	if processed_selection == 0:
 		selector_new_game.text = ">"
 	if processed_selection == 1:
@@ -30,9 +27,20 @@ func set_current_selection(direction):
 	if processed_selection == 2:
 		selector_exit.text = ">"
 
+func handle_selection(_current_selection):	
+	if _current_selection == 0:
+		var main_scene = preload("res://src/Main.tscn").instance()
+		get_tree().get_root().add_child(main_scene)
+		queue_free()
+	if _current_selection == 1:
+		print("TODO: Add options")
+	if _current_selection == 2:
+		get_tree().quit()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up") or event.is_action_pressed("camera_forward"):
 		set_current_selection(1)
 	if event.is_action_pressed("ui_down") or event.is_action_pressed("camera_backward"):
 		set_current_selection(-1)
-	
+	if event.is_action_pressed("ui_accept"):
+		handle_selection(current_selection)
